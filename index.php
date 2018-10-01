7a8d827eb03c0ed67af9bfeecb10822fa2dc2487
Jacob Ewing <jacob.ewing@curling.ca>
2018-10-01 14:59:28 -0400

    Better structure for index.php

--- index.php
+++ index.php
@@ -1,48 +1,45 @@
 <?php
-	if(array_key_exists('getPHPDungeon', $_GET)){
-		include("generator.php");
-		$size = rand() % 60;
-		$map = buildDungeon(array(
-			'width' => 30 + $size, 
-			'height' => 30 + $size,
-			'stairdown' => true,
-			'stairup' => true
-			//'roomscale' => .6
-		));
-		for($y = 0; $y < count($map[0]); $y++){
-			for($x = 0; $x < count($map); $x++){
-				echo $map[$x][$y];
-			}
-			echo "\n";
+	if(array_key_exists('phpbuild', $_GET)){
+		if(!array_key_exists('category', $_GET)){
+			throw new Exception("No map category specified");
 		}
-		echo "\n";
-		exit;
-	}
-
-	if(array_key_exists('getPHPForest', $_GET)){
-		include("generator.php");
-		$size = rand() % 60;
-		$map = buildForest(array(
-			'width' => 30 + $size,
-			'height' => 30 + $size
-		));
-		for($y = 0; $y < count($map[0]); $y++){
-			for($x = 0; $x < count($map); $x++){
-				echo $map[$x][$y];
-			}
-			echo "\n";
+		if(!in_array($_GET['category'], array('dungeon', 'forest', 'swamp'))){
 		}
-		echo "\n";
-		exit;
-	}
 
-	if(array_key_exists('getPHPSwamp', $_GET)){
+
+
 		include("generator.php");
+
 		$size = rand() % 60;
-		$map = buildSwamp(array(
-			'width' => 30 + $size,
-			'height' => 30 + $size
-		));
+		
+		switch($_GET['category']){
+			case 'dungeon':
+				$map = buildDungeon(array(
+					'width' => 30 + $size, 
+					'height' => 30 + $size,
+					'stairdown' => true,
+					'stairup' => true
+					//'roomscale' => .6
+				));
+				break;
+			case 'swamp':
+				$map = buildSwamp(array(
+					'width' => 30 + $size, 
+					'height' => 30 + $size
+				));
+				break;
+			case 'forest':
+				$map = buildForest(array(
+					'width' => 30 + $size, 
+					'height' => 30 + $size
+				));
+				break;
+			default:
+				throw new Exception("Invalid map type specified");
+
+		}
+
+
 		for($y = 0; $y < count($map[0]); $y++){
 			for($x = 0; $x < count($map); $x++){
 				echo $map[$x][$y];
@@ -70,67 +67,44 @@
 	<script type="text/javascript" src="jquery.min.js"></script>
 	<script type="text/javascript" src="generator.js"></script>
 	<script type="text/javascript">
-			function phpDungeon(){
-			$('#mapContent').load('index.php?getPHPDungeon');
+		function phpBuild(category){
+			$('#mapContent').load('index.php?phpbuild&category=' + category);
 		}
 
-		function phpForest(){
-			$('#mapContent').load('index.php?getPHPForest');
-		}
-
-		function phpSwamp(){
-			$('#mapContent').load('index.php?getPHPSwamp');
-		}
-
-		function jsForest(){
+		function jsBuild(mapType){
 			var size = Math.floor(Math.random() * 60);
-			map = buildForest({
-				'width' : 30 + size, 
-				'height' : 30 + size
-			});
-			var lineStr;
-			$('#mapContent').html('');
-			for(var y = 0; y < map.length; y++){
-				lineStr = "";
-				for(var x = 0; x < map[y].length; x++){
-					lineStr += map[x][y];
-				}
-				$('#mapContent').append(lineStr + "\n");
+			switch(mapType){
+				case 'dungeon':
+					map = buildDungeon({
+						'width' : 30 + size, 
+						'height' : 30 + size,
+						'stairdown' : true,
+						'stairup' : true
+						//'roomscale' : 1
+					})
+					break;
+				case 'swamp':
+					map = buildSwamp({
+						'width' : 30 + size, 
+						'height' : 30 + size
+					});
+					break;
+				case 'forest':
+					map = buildForest({
+						'width' : 30 + size, 
+						'height' : 30 + size
+					});
+					break;
+				default:
+					alert('Invalid map type');
+					map = [];
 			}
-		}
 
-		function jsSwamp(){
-			var size = Math.floor(Math.random() * 60);
-			map = buildSwamp({
-				'width' : 30 + size, 
-				'height' : 30 + size
-			});
 			var lineStr;
 			$('#mapContent').html('');
-			for(var y = 0; y < map.length; y++){
+			for(var y = 0; y < map[0].length; y++){
 				lineStr = "";
-				for(var x = 0; x < map[y].length; x++){
-					lineStr += map[x][y];
-				}
-				$('#mapContent').append(lineStr + "\n");
-			}
-		}
-
-		function jsDungeon(){
-			//$('#mapContent').load('index.php?getPHPMap');
-			var size = Math.floor(Math.random() * 60);
-			map = buildDungeon({
-				'width' : 30 + size, 
-				'height' : 30 + size,
-				'stairdown' : true,
-				'stairup' : true
-				//'roomscale' : 1
-			});
-			var lineStr;
-			$('#mapContent').html('');
-			for(var y = 0; y < map.length; y++){
-				lineStr = "";
-				for(var x = 0; x < map[y].length; x++){
+				for(var x = 0; x < map.length; x++){
 					lineStr += map[x][y];
 				}
 				$('#mapContent').append(lineStr + "\n");
@@ -139,12 +113,12 @@
 	</script>
 </head>
 <body>
-	<button onclick="phpDungeon();">PHP Dungeon</button>
-	<button onclick="phpForest();">PHP Forest</button>
-	<button onclick="phpSwamp();">PHP Swamp</button>
-	<button onclick="jsDungeon();">JS Dungeon</button>
-	<button onclick="jsForest();">JS Forest</button>
-	<button onclick="jsSwamp();">JS Swamp</button>
+	<button onclick="phpBuild('dungeon');">PHP Dungeon</button>
+	<button onclick="phpBuild('forest');">PHP Forest</button>
+	<button onclick="phpBuild('swamp');">PHP Swamp</button>
+	<button onclick="jsBuild('dungeon');">JS Dungeon</button>
+	<button onclick="jsBuild('forest');">JS Forest</button>
+	<button onclick="jsBuild('swamp');">JS Swamp</button>
 	<pre id="mapContent">
 </pre>
 </body>
